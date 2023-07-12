573636c236572cdaafe32ac6bd8b5d8de680ba82
Steve Harris <doc@steveharris.me>
2023-07-12 09:43:19 +0100

    streamlit on docker mwe

--- web/app.py
+++ web/app.py
@@ -1,25 +1,38 @@
-from flask import Flask, jsonify
-import requests
-import os
+from collections import namedtuple
+import altair as alt
+import math
+import pandas as pd
+import streamlit as st
 
-app = Flask(__name__)
+"""
+# Welcome to Streamlit!
 
-# NB: when running this with docker compose then must use the service name 'api'
-# not 'localhost'
-API_PORT = os.environ.get("API_PORT", 8401)
-BACKEND_URL = f"http://api:{API_PORT}"
+Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
 
+If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
+forums](https://discuss.streamlit.io).
 
-@app.route('/')
-def index():
-    response = requests.get(f"{BACKEND_URL}/")
-    return jsonify(response.json()), response.status_code
-
-@app.route("/names/<string:name>")
-def get_name(name):
-    response = requests.get(f"{BACKEND_URL}/names/{name}")
-    return jsonify(response.json()), response.status_code
+In the meantime, below is an example of what you can do with just a few lines of code:
+"""
 
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8300, debug=True)
+with st.echo(code_location='below'):
+    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
+    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+
+    Point = namedtuple('Point', 'x y')
+    data = []
+
+    points_per_turn = total_points / num_turns
+
+    for curr_point_num in range(total_points):
+        curr_turn, i = divmod(curr_point_num, points_per_turn)
+        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
+        radius = curr_point_num / total_points
+        x = radius * math.cos(angle)
+        y = radius * math.sin(angle)
+        data.append(Point(x, y))
+
+    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
+        .mark_circle(color='#0068c9', opacity=0.5)
+        .encode(x='x:Q', y='y:Q'))
