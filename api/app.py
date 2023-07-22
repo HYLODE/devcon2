3a1146a8c56b40eb9d5479acf9e04d3a9998848d
Steve Harris <git@steveharris.me>
2023-07-22 19:27:11 +0100

    demo data at API

--- api/app.py
+++ api/app.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+import requests
+import json
 
 # Create the app
 app = FastAPI()
@@ -11,4 +13,31 @@ def index():
 @app.get("/names/{name}")
 def names(name: str):
     return {"Your name is": name}
+
+
+@app.get("/discharges/predictions")
+def discharges_predictions():
+    """
+    Returns a list of discharge predictions.
+    http://uclvlddpragae08:5691/predictions/
+    ```
+        [
+        {
+            "event_timestamp": "2023-07-22T19:04:57",
+            "note": "foo bar",
+            "discharge_probability": 1.0
+        },
+        ...
+        ]
+    ```
+    """
+    try:
+        response = requests.get("http://uclvlddpragae08:5691")
+        data = response.json()
+    except requests.exceptions.ConnectionError as e:
+        print(e)
+        print("Will try with local data")
+        with open("data/test/discharges.json") as f:
+            data = json.load(f)
+    return data
 
